3afc2b9da5bf2396afcf94d11eaaeb38a5244d37
Muhammad Junaidi <emje662@gmail.com>
2024-09-15 08:48:11 +0700

    mengedit kolom output menjadi menggunakan elemen div dan span.

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-function toCupang(letter) {
+function translate(letter) {
   const kamus = [
-    ["A", "BLUK"],
-    ["B", "BUL"],
-    ["C", "BULK"],
-    ["D", "BLUB"],
-    ["E", "BLUKBLUK"],
-    ["F", "BLUKBUL"],
-    ["G", "BLUKBULK"],
-    ["H", "BLUKBLUB"],
-    ["I", "BULBLUK"],
-    ["J", "BULBUL"],
-    ["K", "BULBULK"],
-    ["L", "BULBLUB"],
-    ["M", "BULKBLUK"],
-    ["N", "BULKBUL"],
-    ["O", "BULKBULK"],
-    ["P", "BULKBLUB"],
-    ["Q", "BLUBBLUK"],
-    ["R", "BLUBBUL"],
-    ["S", "BLUBBULK"],
-    ["T", "BLUBBLUB"],
-    ["U", "BLUKBLUKBLUK"],
-    ["V", "BLUKBLUKBUL"],
-    ["W", "BLUKBLUKBULK"],
-    ["X", "BLUKBLUKBLUB"],
-    ["Y", "BLUKBULBLUK"],
-    ["Z", "BLUKBULBUL"],
+    ["A", "BLUK_"],
+    ["B", "BUL_"],
+    ["C", "BULK_"],
+    ["D", "BLUB_"],
+    ["E", "BLUKBLUK_"],
+    ["F", "BLUKBUL_"],
+    ["G", "BLUKBULK_"],
+    ["H", "BLUKBLUB_"],
+    ["I", "BULBLUK_"],
+    ["J", "BULBUL_"],
+    ["K", "BULBULK_"],
+    ["L", "BULBLUB_"],
+    ["M", "BULKBLUK_"],
+    ["N", "BULKBUL_"],
+    ["O", "BULKBULK_"],
+    ["P", "BULKBLUB_"],
+    ["Q", "BLUBBLUK_"],
+    ["R", "BLUBBUL_"],
+    ["S", "BLUBBULK_"],
+    ["T", "BLUBBLUB_"],
+    ["U", "BLUKBLUKBLUK_"],
+    ["V", "BLUKBLUKBUL_"],
+    ["W", "BLUKBLUKBULK_"],
+    ["X", "BLUKBLUKBLUB_"],
+    ["Y", "BLUKBULBLUK_"],
+    ["Z", "BLUKBULBUL_"],
   ];
 
   for (let i = 0; i < kamus.length; i++) {
     if (letter == kamus[i][0]) {
       return kamus[i][1];
+    } else if (letter == "_") {
+      return "";
     }
   }
   return letter;
 }
 
-const kolomIndo = document.getElementById("b-indo");
+// Select element input & output
+const inputEl = document.getElementById("input-text");
+const ouputEl = document.getElementById("output-text");
 
-const kolomCupang = document.getElementById("b-cupang");
-
-kolomIndo.addEventListener("input", function () {
-  const bIndo = kolomIndo.value.toUpperCase();
-  const bCupang = bIndo.split("");
-  const result = bCupang.map(function (huruf) {
-    return toCupang(huruf);
+inputEl.addEventListener("input", function () {
+  const inputText = inputEl.value.toUpperCase();
+  const arrInput = inputText.split("");
+  const ouputText = arrInput.map(function (huruf) {
+    return translate(huruf);
   });
-
-  kolomCupang.value = result.join("");
-  console.log("berhasil sih");
+  ouputEl.textContent = ouputText.join("");
 });
